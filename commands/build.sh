b86452c4a680d7e3549fe1c432a5499b9a014d3e
Samuel McBroom <sam@sammcb.com>
2024-12-01 13:23:53 -0800

    fix

--- commands/build.sh
+++ commands/build.sh
@@ -3,7 +3,6 @@ nix run .#clean
 mkdir "${build_directory}"
 
 datapack_directory="datapack"
-resourcepack_directory="resourcepack"
 
 if [ -d "${datapack_directory}" ]; then
 	datapack_zip_name="${datapack_name}-${version}-${minecraft_version}.zip"
@@ -13,16 +12,5 @@ if [ -d "${datapack_directory}" ]; then
 	(
 		cd ${datapack_directory} || exit 1
 		zip -q -r "../${build_directory}/${datapack_zip_name}" data pack.mcmeta pack.png
-	)
-fi
-
-if [ -d "${resourcepack_directory}" ]; then
-	resourcepack_zip_name="${resourcepack_name} ${version} ${minecraft_version}.zip"
-
-	echo "Zipping resourcepack '${resourcepack_zip_name}' into build directory '${build_directory}'..."
-
-	(
-		cd ${resourcepack_directory} || exit 1
-		zip -q -r "../${build_directory}/${resourcepack_zip_name}" assets pack.mcmeta pack.png
 	)
 fi
